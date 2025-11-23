e0774018b8339f28bfe084d677acc4565d957e9f
Satoko <81878473+s-y-hub@users.noreply.github.com>
2025-11-23 11:17:41 +0900

    calculate.java

---
+++ calculate.java
@@ -0,0 +1,35 @@
+//It's been a while to write Java code since I graduate school
+public class Main {
+// Variables
+int number1 = 1;
+int number2 =2;
+int sum;
+int sub;
+int mul;
+int div;
+
+  
+// Sum
+sum = number1+number2;
+
+System.out.println=("Sum: " + sum);
+
+
+// Sub
+sub = number2-number1;
+
+System.out.println=("Sub: " + sub);
+
+
+// Mul
+mul = number1*number2;
+
+System.out.println=("Mul: " + mul);
+
+
+// Div
+div = number1+number2;
+
+System.out.println=("Div: " + div);
+
+}
